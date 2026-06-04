a7a67a7c36445ecfb0e230419d569e6db2d01e43
OmRana2004 <omrana8755800322@gmail.com>
2026-06-05 03:03:54 +0530

    Depolyed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://task-management-web-application-6odg.onrender.com/api/v1",
 });
 
 API.interceptors.request.use((config) => {
